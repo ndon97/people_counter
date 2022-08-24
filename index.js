db7f49556f39548717e22511461173d2a3ad22d8
Nick Don <nick.don.1102@gmail.com>
2022-08-24 22:10:01 +1000

    Basic javascript learning with incremenet counter

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.getElementById("count").innerText = 5
+let countEl = document.getElementById("count")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+  count += 1
+  countEl.textContent = count
+}
+
+function save () {
+  let currentCount = count + " - "
+  saveEl.textContent += currentCount
+  countEl.textContent = 0
+  count = 0
+}
